0b56045bc0856017e079909686c581b3315b39b3
Artur <arturworkit@gmail.com>
2022-11-22 14:13:41 +0200

    Finish Login page

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,9 +1,9 @@
 import Form from "../components/global/Form";
 import {useState} from "react";
 import {useDispatch} from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { useLoginMutation } from "../lib/store/auth/auth-slice-api";
-import { setCredendials } from "../lib/store/auth/auth-slice";
+import {useNavigate} from "react-router-dom";
+import {useLoginMutation, useRegisterMutation} from "../lib/store/auth/auth-slice-api";
+import {setCredendials} from "../lib/store/auth/auth-slice";
 
 export default function Login() {
     const fields = [
@@ -25,21 +25,30 @@ export default function Login() {
         }
     ]
     const [error, setError] = useState(null)
-    const [login, {isLoading}] = useLoginMutation()
+    const [loading, setLoading] = useState(false)
+    const [login] = useLoginMutation()
+    const [register] = useRegisterMutation()
     const dispatch = useDispatch()
     const navigate = useNavigate()
+    const path = window.location.pathname
+    const title = path === '/login' ? 'Sign In' : 'Sign Up'
     const onSubmit = async (data) => {
         try {
-            const response = await login(data).unwrap()
+            setLoading(true)
+            const response = path === '/login' ? await login(data).unwrap() : await register(data).unwrap()
             dispatch(setCredendials(response))
             navigate('/')
         } catch (error) {
-            setError(error.data)
+            setError(error?.data)
+        } finally {
+            setLoading(false)
         }
     }
     return (
-        <div className='container-sm'>
-           <Form fields={fields} submitFunction={onSubmit} error={error} loading={isLoading}/>
+        <div className='container-fluid mt-5 d-flex justify-content-center align-items-center'>
+            <Form fields={fields} title={title} submitFunction={onSubmit} error={error} loading={loading}>
+                {path === '/login' && <button onClick={() => navigate('/register')} type='button' className='btn btn-outline-primary w-100'>Sign Up</button>}
+            </Form>
         </div>
     )
 }
