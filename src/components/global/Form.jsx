39cd0739b460471e58f2928feed62af0d7770f7e
Artur <arturworkit@gmail.com>
2022-11-22 14:13:17 +0200

    Add children usage and upgrade styling

--- src/components/global/Form.jsx
+++ src/components/global/Form.jsx
@@ -1,7 +1,7 @@
 import {useForm} from "react-hook-form";
 import ErrorOrLoading from "./ErrorOrLoading";
 
-export default function Form({fields, submitFunction, error, loading}) {
+export default function Form({fields, title, submitFunction, error, loading, children}) {
     const {register, formState: {errors}, handleSubmit, reset} = useForm({mode: "onSubmit"})
     const onSubmit = data => {
         submitFunction(data)
@@ -9,34 +9,45 @@ export default function Form({fields, submitFunction, error, loading}) {
     const onCancel = () => {
         reset()
     }
-    return (<form onSubmit={handleSubmit(onSubmit)} className='container-fluid m-3'>
-        {fields.map(field =>
-            <div key={field.name} className='mb-3'>
-                <input
-                    className={['form-control', errors[field.name] ? 'is-invalid' : null].join(' ')}
-                    type={field.type}
-                    placeholder={field.placeholder}
-                    {...register(field.name,
-                        {
-                            required: {value: field.required, message: `${field.label} is required`},
-                            min: {value: field.min, message: `Minimum ${field.label} value is ${field.min}`},
-                            max: {value: field.max, message: `Maximum ${field.label} value is ${field.max}`},
-                            minLength: {
-                                value: field.minLength,
-                                message: `Minimum ${field.label} length is ${field.minLength}`
-                            },
-                            maxLength: {
-                                value: field.maxLength,
-                                message: `Maximum ${field.label} length is ${field.maxLength}`
-                            },
-                        })} />
-                <div className='text-danger'>{errors?.[field.name]?.message}</div>
-            </div>
-        )}
-        <ErrorOrLoading error={error} loading={loading}/>
-        <div className='btn-group gap-3'>
-            <button className='btn btn-outline-success' type='submit'>Submit</button>
-            <button className='btn btn-outline-danger' onClick={onCancel} type='reset'>Cancel</button>
-        </div>
-    </form>)
+    return (
+        <div className='card w-50'>
+            {title && <div className='card-header font-monospace card-title h2'>{title}</div>}
+            <form onSubmit={handleSubmit(onSubmit)} className='card-body'>
+                {fields.map(field =>
+                    <div key={field.name} className='mb-3'>
+                        <input
+                            className={['form-control', errors[field.name] ? 'is-invalid' : null].join(' ')}
+                            type={field.type}
+                            placeholder={field.placeholder}
+                            {...register(field.name,
+                                {
+                                    required: {value: field.required, message: `${field.label} is required`},
+                                    min: {
+                                        value: field.min,
+                                        message: `Minimum ${field.label} value is ${field.min}`
+                                    },
+                                    max: {
+                                        value: field.max,
+                                        message: `Maximum ${field.label} value is ${field.max}`
+                                    },
+                                    minLength: {
+                                        value: field.minLength,
+                                        message: `Minimum ${field.label} length is ${field.minLength}`
+                                    },
+                                    maxLength: {
+                                        value: field.maxLength,
+                                        message: `Maximum ${field.label} length is ${field.maxLength}`
+                                    },
+                                })} />
+                        <div className='text-danger'>{errors?.[field.name]?.message}</div>
+                    </div>
+                )}
+                <ErrorOrLoading error={error} loading={loading}/>
+                <div className='card-footer bg-transparent d-flex justify-content-evenly align-items-center'>
+                    <button className='btn btn-outline-success' type='submit'>Submit</button>
+                    <button className='btn btn-outline-danger' onClick={onCancel} type='reset'>Cancel</button>
+                </div>
+                {children && <div className='card-footer bg-transparent'>{children}</div>}
+            </form>
+        </div>)
 }
