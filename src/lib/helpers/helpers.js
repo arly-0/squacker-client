ea8087454e84846463a72ea05fc678866b74bb0b
Artur <arturworkit@gmail.com>
2022-11-23 10:36:45 +0200

    Add date and bool formatters

--- src/lib/helpers/helpers.js
+++ src/lib/helpers/helpers.js
@@ -1,6 +1,19 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCheck, faXmark} from "@fortawesome/free-solid-svg-icons";
+
 export const tabToUrl = (tab) => {
     if(tab === 'Home')
         return '/'
     else
         return `/${tab.toLowerCase()}`
+}
+
+export const formatDate = date => {
+    if(date) return new Date(date).toDateString()
+    else return 'Not specified'
+}
+export const formatBool = bool => {
+    if(bool === true) return <FontAwesomeIcon icon={faCheck} />
+    else if (bool === false) return <FontAwesomeIcon icon={faXmark}/>
+    else return 'Not specified'
 }
