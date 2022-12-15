f85e66fb5470b6c689639133d7caec7312791d4d
Artur <arturworkit@gmail.com>
2022-12-15 21:29:23 +0200

    Add dynamic styling to form component

--- src/components/global/Form.jsx
+++ src/components/global/Form.jsx
@@ -2,10 +2,23 @@ import {useForm} from "react-hook-form";
 import ErrorOrLoading from "./ErrorOrLoading";
 import {useState} from "react";
 
-export default function Form({fields, title, submitFunction, children}) {
+export default function Form({fields, title, submitFunction, children, onCancel}) {
     const {register, formState: {errors}, handleSubmit, reset} = useForm({mode: "onSubmit"})
     const [error, setError] = useState(null)
     const [loading, setLoading] = useState(false)
+    const fieldClass = (field) => {
+        let defaultClass
+        switch (field.type) {
+            case 'checkbox':
+                defaultClass = 'form-check-input d-flex'
+                break
+            default:
+                defaultClass = 'form-control'
+                break
+        }
+        return [defaultClass,
+            errors[field.name] ? 'is-invalid' : null].join(' ')
+    }
     const onSubmit = data => {
         setLoading(true)
         submitFunction(data).catch(error => {
@@ -14,24 +27,26 @@ export default function Form({fields, title, submitFunction, children}) {
             setLoading(false)
         })
     }
-    const onCancel = () => {
+    const cancelHandler = () => {
         reset()
         setError(null)
         setLoading(false)
+        onCancel()
     }
     return (
-        <div className='card w-50'>
+        <div className='card'>
             {title && <div className='card-header font-monospace card-title h2'>{title}</div>}
             <form onSubmit={handleSubmit(onSubmit)} className='card-body'>
                 {fields.map(field =>
                     <div key={field.name} className='mb-3'>
                         {field.label && <label className='form-label'>{field.label}</label>}
                         <input
-                            className={['form-control', errors[field.name] ? 'is-invalid' : null].join(' ')}
+                            className={fieldClass(field)}
                             type={field.type}
                             placeholder={field.placeholder}
                             {...register(field.name,
                                 {
+                                    value: field.value,
                                     required: {value: field.required, message: `${field.label} is required`},
                                     min: {
                                         value: field.min,
@@ -56,7 +71,7 @@ export default function Form({fields, title, submitFunction, children}) {
                 <ErrorOrLoading error={error} loading={loading}/>
                 <div className='card-footer bg-transparent d-flex justify-content-evenly align-items-center'>
                     <button className='btn btn-outline-success' type='submit'>Submit</button>
-                    <button className='btn btn-outline-danger' onClick={onCancel} type='reset'>Cancel</button>
+                    <button className='btn btn-outline-danger' onClick={cancelHandler} type='reset'>Cancel</button>
                 </div>
                 {children && <div className='card-footer bg-transparent'>{children}</div>}
             </form>
